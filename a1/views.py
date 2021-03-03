9bfa3a5a60b82376a9be31acc51ae2bddcd6b383
rahulves <79927722+rahulves@users.noreply.github.com>
2021-03-03 19:22:08 +0530

    Update views.py

--- a1/views.py
+++ a1/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
     user_name = request.GET.get('user_name', '')
-    return HttpResponse("Hello, My Name is " + user_name)
+    return HttpResponse("Hello, My Name is " + user_name+user_name)
